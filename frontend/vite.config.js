a7cffa2696513f5156d47a58721dbe86b77d66c4
Crystal Huang <z5215571@ad.unsw.edu.au>
2025-04-14 02:39:01 +1000

    Added styles.css for Tailwind CSS

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,6 +1,7 @@
 import react from '@vitejs/plugin-react'
 import { defineConfig } from 'vite'
 import tailwindcss from '@tailwindcss/vite'
+
 import path from 'path'
 
 // https://vitejs.dev/config/
