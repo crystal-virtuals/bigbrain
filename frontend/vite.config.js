4c69c5325cd9386130f0d9c37d32ff5b17f6a3c4
Crystal Huang <z5215571@ad.unsw.edu.au>
2025-04-14 00:47:37 +1000

    Setup path aliases

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,6 +1,7 @@
 import react from '@vitejs/plugin-react'
 import { defineConfig } from 'vite'
 import tailwindcss from '@tailwindcss/vite'
+import path from 'path'
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -16,4 +17,12 @@ export default defineConfig({
   server: {
     port: 3000,
   },
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, '/src'),
+      '@utils': path.resolve(__dirname, '/src/shared/utils'),
+      '@components': path.resolve(__dirname, '/src/shared/components'),
+      '@auth': path.resolve(__dirname, '/src/Auth'),
+    }
+  }
 })
