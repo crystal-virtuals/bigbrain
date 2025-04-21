b72f7711f6cbdbbf33db6e2822ce21ed4cd063b1
Crystal Huang <z5215571@ad.unsw.edu.au>
2025-04-21 14:12:54 +1000

    updated input component to reflect readonly state

--- frontend/src/shared/components/input.jsx
+++ frontend/src/shared/components/input.jsx
@@ -35,7 +35,7 @@ export const Input = forwardRef(function Input(
         // Basic layout
         'relative block w-full',
         // Background color + shadow applied to inset pseudo element, so shadow blends with border in light mode
-        'before:absolute before:inset-px before:rounded-[calc(var(--radius-lg)-1px)] before:bg-white before:shadow-sm',
+        !readOnly && 'before:absolute before:inset-px before:rounded-[calc(var(--radius-lg)-1px)] before:bg-white before:shadow-sm',
         // Background color is moved to control and shadow is removed in dark mode so hide `before` pseudo
         'dark:before:hidden',
         // Focus ring
