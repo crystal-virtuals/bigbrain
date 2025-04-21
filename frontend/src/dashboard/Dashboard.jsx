97c29712390dd37d477758c88a254e1974fc3893
Crystal Huang <z5215571@ad.unsw.edu.au>
2025-04-21 13:05:55 +1000

    Continued work on dashboard

--- frontend/src/dashboard/Dashboard.jsx
+++ frontend/src/dashboard/Dashboard.jsx
@@ -1,44 +1,35 @@
+import { CreateGameButton, GameCardList } from '@/game/components';
+import { Heading } from '@components/heading';
 import { SidebarLayout } from '@components/sidebar-layout';
-import { useAuth } from '@hooks/auth';
-import { Outlet } from 'react-router-dom';
-import { Navbar, Sidebar } from './components';
+import { useOutletContext } from 'react-router-dom';
+import { Navbar, Sidebar } from '@/admin/components';
 
-/**
- * A dashboard of all games is displayed, where each game shows:
- * (1) the title,
- * (2) number of questions it contains,
- * (3) a thumbnail, and
- * (4) a total duration to complete (sum of each individual question's duration).
- *
- * Each game listed should have a clickable UI component relating to it
- * that takes user to the screen to edit that particular game. E.G. /game/{game_id}
- *
- * A button exists on this screen which brings up a UI component that allows user to create a new game, provide a name for the game.
- * After a new game is created, it must be added to the dashboard immediately without a refresh.
- *
- * A button exists on this screen that brings up a UI component to allow user to delete a particular game.
- */
-function DashboardLayout({ children }) {
-  const { user } = useAuth();
+function DashboardLayout({ user, children }) {
   return (
     <SidebarLayout
       navbar={<Navbar user={user} />}
       sidebar={<Sidebar user={user} />}
     >
-      {children}
+      <div className="container mx-auto mt-6">{children}</div>;
     </SidebarLayout>
   );
 }
 
-
-function Dashboard() {
-  // fetch games on load?
+function DashboardHeading({ createGame }) {
   return (
-    <DashboardLayout>
-      {/* This is where the nested routes will be rendered */}
-      <Outlet />
-    </DashboardLayout>
+    <div className="flex w-full flex-wrap items-end justify-between gap-4 border-b border-zinc-950/10 pb-6 dark:border-white/10">
+      <Heading>Dashboard</Heading>
+      <CreateGameButton onCreate={createGame} />
+    </div>
   );
 }
 
-export default Dashboard;
+export default function Dashboard() {
+  const { user, games, createGame, deleteGame } = useOutletContext();
+  return (
+    <DashboardLayout user={user}>
+      <DashboardHeading createGame={createGame} />
+      <GameCardList games={games} onDelete={deleteGame} onCreate={createGame}/>
+    </DashboardLayout>
+  );
+}
