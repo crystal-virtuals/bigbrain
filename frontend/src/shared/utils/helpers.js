f4f9f57e4aba9aa4d7a171f00ec11cd69e8b2a7c
Crystal Huang <z5215571@ad.unsw.edu.au>
2025-04-21 14:06:02 +1000

    new file for helper functions

--- frontend/src/shared/utils/helpers.js
+++ frontend/src/shared/utils/helpers.js
@@ -1,4 +1,39 @@
 /***************************************************************
+                      Data Input
+***************************************************************/
+/**
+ * Given a js file object representing a jpg or png image, such as one taken
+ * from a html file input element, return a promise which resolves to the file
+ * data as a data url.
+ * More info:
+ *   https://developer.mozilla.org/en-US/docs/Web/API/File
+ *   https://developer.mozilla.org/en-US/docs/Web/API/FileReader
+ *   https://developer.mozilla.org/en-US/docs/Web/HTTP/Basics_of_HTTP/Data_URIs
+ *
+ * Example Usage:
+ *   const file = document.querySelector('input[type="file"]').files[0];
+ *   console.log(fileToDataUrl(file));
+ * @param {File} file The file to be read.
+ * @return {Promise<string>} Promise which resolves to the file as a data url.
+ */
+export function fileToDataUrl(file) {
+  const validFileTypes = [ 'image/jpeg', 'image/png', 'image/jpg' ]
+  const valid = validFileTypes.find(type => type === file.type);
+  // Bad data, let's walk away.
+  if (!valid) {
+    throw Error('Invalid file type. Please upload a PNG, JPG, or JPEG.');
+  }
+
+  const reader = new FileReader();
+  const dataUrlPromise = new Promise((resolve,reject) => {
+    reader.onerror = reject;
+    reader.onload = () => resolve(reader.result);
+  });
+  reader.readAsDataURL(file);
+  return dataUrlPromise;
+}
+
+/***************************************************************
                        String Processing
 ***************************************************************/
 export const pluralSuffix = (count) => count > 1 ? 's' : '';
