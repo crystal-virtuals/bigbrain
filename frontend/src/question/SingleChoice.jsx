a31c5b3443a06fde147e20e9cd232b64636f7543
Crystal Huang <z5215571@ad.unsw.edu.au>
2025-04-28 00:01:37 +1000

    Continued work on Feature 2.2.2. Edit BigBrain Game

--- frontend/src/question/SingleChoice.jsx
+++ frontend/src/question/SingleChoice.jsx
@@ -2,26 +2,17 @@ import { ControlledInput, LabelTab } from '@components/form';
 import { Field } from '@components/fieldset';
 import { Button } from '@components/button'
 import { PlusIcon } from '@heroicons/react/16/solid'
-import { newAnswer, isEqual, mapToAnswer } from '@utils/game';
-import { isEmptyString, pluralSuffix } from '@utils/helpers';
+import { newAnswer, isEqual } from '@utils/game';
 
 // One correct answer
 export default function SingleChoice({ answers, setAnswers, readOnly, errors, setErrors }) {
-  let correct = answers.find(a => a.correct);    // single (1) correct answer
-  let falses = answers.filter(a => !a.correct);  // multiple (1-5) false answers
-
-  if (!correct) {
-    correct = newAnswer(true);
-    setAnswers(prev => [...prev, correct]);
-  }
-
-  if (falses.length === 0) {
-    falses = [newAnswer(false)];
-    setAnswers(prev => [...prev, falses[0]]);
-  }
+  let correct = answers.find((a) => a.correct); // single (0-1) correct answer
+  let falses = answers.filter((a) => !a.correct); // multiple (1-5) false answers
 
   const updateAnswer = (answer) => {
-    const updatedAnswers = answers.map(a => isEqual(a, answer.id) ? answer : a);
+    const updatedAnswers = answers.map((a) =>
+      isEqual(a, answer.id) ? answer : a
+    );
     setAnswers(updatedAnswers);
   };
 
@@ -29,12 +20,17 @@ export default function SingleChoice({ answers, setAnswers, readOnly, errors, se
     const newFalseAnswer = newAnswer(false);
     const updatedAnswers = [...answers, newFalseAnswer];
     setAnswers(updatedAnswers);
-  }
+  };
 
   return (
     <>
       <Field className="col-span-full">
-        <LabelTab type="correct" label="Correct answer" htmlFor="correctAnswer" invalid={errors.has("correctAnswer")}>
+        <LabelTab
+          type="correct"
+          label="Correct answer"
+          htmlFor="correctAnswer"
+          invalid={errors.has('correctAnswer') && !readOnly}
+        >
           <ControlledInput
             type="text"
             name="correctAnswer"
@@ -43,8 +39,9 @@ export default function SingleChoice({ answers, setAnswers, readOnly, errors, se
             setValue={(value) => updateAnswer({ ...correct, name: value })}
             errors={errors}
             setErrors={setErrors}
-            inputclassname={readOnly && 'cursor-default pointer-events-none'}
             errorMessage="Correct answer cannot be empty"
+            className={readOnly && 'before:opacity-90'}
+            inputclassname={readOnly && 'cursor-default pointer-events-none'}
             correct
             required
           />
@@ -52,7 +49,12 @@ export default function SingleChoice({ answers, setAnswers, readOnly, errors, se
       </Field>
 
       <Field className="col-span-full">
-        <LabelTab type="false" label="False answers" htmlFor="falseAnswers" invalid={errors.has("falseAnswers")}>
+        <LabelTab
+          type="false"
+          label="False answers"
+          htmlFor="falseAnswers"
+          invalid={errors.has('falseAnswers') && !readOnly}
+        >
           {falses.map((a, index) => (
             <ControlledInput
               type="text"
@@ -66,19 +68,25 @@ export default function SingleChoice({ answers, setAnswers, readOnly, errors, se
               errorMessage="At least one false answer is required"
               correct={false}
               required={index === 0} // only the first false answer is required
+              className={readOnly && 'before:opacity-90'}
               inputclassname={readOnly && 'cursor-default pointer-events-none'}
             />
           ))}
 
           {/* Add at most 5 false answers */}
-          {falses.length < 5 && (
-            <Button type="button" onClick={addAnswer} className="col-span-full mt-2">
+          {answers.length < 6 && !readOnly && (
+            <Button
+              type="button"
+              onClick={addAnswer}
+              className="col-span-full mt-2"
+              disabled={readOnly}
+            >
               <PlusIcon />
               Add another accepted answer
             </Button>
           )}
-        </LabelTab>
 
+        </LabelTab>
       </Field>
     </>
   );
