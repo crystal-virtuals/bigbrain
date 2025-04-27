89e22b019aa06f2b1bdd50ef8c7fbd355ca97661
Crystal Huang <z5215571@ad.unsw.edu.au>
2025-04-28 00:01:41 +1000

    Continued work on Feature 2.2.2. Edit BigBrain Game

--- frontend/src/question/QuestionControls.jsx
+++ frontend/src/question/QuestionControls.jsx
@@ -6,21 +6,41 @@ import {
   ListboxLabel,
   ListboxOption,
 } from '@components/listbox';
-import {
-  questionTypeOptions,
-  timeLimitOptions,
-} from '@constants/question';
+import { questionTypeOptions, durationOptions } from '@constants/question';
+import clsx from 'clsx';
 
-export function InputQuestionName({ question, setQuestion, readOnly, errors, setErrors }) {
+function ReadonlyInput({ className, children }) {
+  const styles = [
+    'text-base/6 text-zinc-950 sm:text-sm/6 py-[calc(--spacing(2.5)-1px)] sm:py-[calc(--spacing(1.5)-1px)] dark:text-white',
+  ];
+  return <div className={clsx(styles, className)}>{children}</div>;
+}
+
+export function InputQuestionName({
+  question,
+  setQuestion,
+  readOnly,
+  errors,
+  setErrors,
+}) {
+
   return (
     <Field className="col-span-full">
-      <LabelTab type="neutral" label="Question" htmlFor="name" invalid={errors.has("name")}>
+      <LabelTab
+        type="neutral"
+        label="Question"
+        htmlFor="name"
+        invalid={errors.has('name') && !readOnly}
+        className={clsx(readOnly && 'cursor-default pointer-events-none')}
+      >
         <ControlledTextarea
           id="name"
           name="name"
           rows="2"
           value={question.name}
-          onChange={(e) => setQuestion((prev) => ({ ...prev, name: e.target.value }))}
+          onChange={(e) =>
+            setQuestion((prev) => ({ ...prev, name: e.target.value }))
+          }
           errors={errors}
           setErrors={setErrors}
           readOnly={readOnly}
@@ -32,43 +52,68 @@ export function InputQuestionName({ question, setQuestion, readOnly, errors, set
   );
 }
 
-export function SelectQuestionType({ question, setQuestion }) {
+export function SelectQuestionType({ question, setQuestion, readOnly }) {
   return (
     <Field className="col-span-full">
       <Label>Question Type</Label>
-      <Listbox
-        name="questionType"
-        value={question.type}
-        onChange={(e) => setQuestion((prev) => ({ ...prev, type: e.target.value }))}
-      >
-        {questionTypeOptions.map((option) => (
-          <ListboxOption key={option.value} value={option.value}>
-            <ListboxLabel>{option.label}</ListboxLabel>
-            {option.description && (
-              <ListboxDescription>{option.description}</ListboxDescription>
-            )}
-          </ListboxOption>
-        ))}
-      </Listbox>
+      {readOnly ? (
+        <ReadonlyInput>
+          {
+            questionTypeOptions.find((option) => option.value === question.type)
+              ?.label
+          }
+        </ReadonlyInput>
+      ) : (
+        <Listbox
+          name="questionType"
+          value={question.type}
+          onChange={(e) =>
+            setQuestion((prev) => ({ ...prev, type: e.target.value }))
+          }
+        >
+          {questionTypeOptions.map((option) => (
+            <ListboxOption key={option.value} value={option.value}>
+              <ListboxLabel>{option.label}</ListboxLabel>
+              {option.description && (
+                <ListboxDescription>{option.description}</ListboxDescription>
+              )}
+            </ListboxOption>
+          ))}
+        </Listbox>
+      )}
     </Field>
   );
 }
 
-export function SelectTimeLimit({ question, setQuestion }) {
+export function SelectDuration({ question, setQuestion, readOnly }) {
   return (
     <Field className="col-span-full">
-      <Label htmlFor='timeLimit'>Time Limit</Label>
-      <Listbox
-        name="timeLimit"
-        value={question.timeLimit}
-        onChange={(e) => setQuestion((prev) => ({ ...prev, timeLimit: Number(e.target.value) }))}
-      >
-        {timeLimitOptions.map((option) => (
-          <ListboxOption key={option.value} value={option.value}>
-            <ListboxLabel>{option.label}</ListboxLabel>
-          </ListboxOption>
-        ))}
-      </Listbox>
+      <Label htmlFor="duration">Time Limit</Label>
+      {readOnly ? (
+        <ReadonlyInput>
+          {
+            durationOptions.find((option) => option.value === question.duration)
+              ?.label
+          }
+        </ReadonlyInput>
+      ) : (
+        <Listbox
+          name="duration"
+          value={question.duration}
+          onChange={(e) =>
+            setQuestion((prev) => ({
+              ...prev,
+              duration: Number(e.target.value),
+            }))
+          }
+        >
+          {durationOptions.map((option) => (
+            <ListboxOption key={option.value} value={option.value}>
+              <ListboxLabel>{option.label}</ListboxLabel>
+            </ListboxOption>
+          ))}
+        </Listbox>
+      )}
     </Field>
   );
 }
