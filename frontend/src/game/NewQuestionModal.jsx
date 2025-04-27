4fc82f232b924b131e3eb3a8c29e8285c3c681e2
Crystal Huang <z5215571@ad.unsw.edu.au>
2025-04-28 00:01:01 +1000

    Continued work on Feature 2.2.2. Edit BigBrain Game

---
+++ frontend/src/game/NewQuestionModal.jsx
@@ -0,0 +1,112 @@
+import mcIcon from '@assets/multiple-choice.svg';
+import scIcon from '@assets/single-choice.svg';
+import tfIcon from '@assets/true-false.png';
+import { Button } from '@components/button';
+import {
+  Dialog,
+  DialogActions,
+  DialogBody,
+  DialogDescription,
+  DialogTitle,
+} from '@components/dialog';
+import { Heading } from '@components/heading';
+import { Text } from '@components/text';
+import { questionTypes } from '@constants/question';
+import clsx from 'clsx';
+
+const styles = {
+  default: [
+    // Cursor
+    'cursor-pointer touch-manipulation pointer-events-auto',
+  ],
+  questionType: [
+    // Base
+    'bg-zinc-400/10 dark:bg-white/10 flex flex-row gap-4 py-2 px-4 md:py-4 rounded-lg mx-0 w-full items-center',
+    // Hover
+    'hover:bg-zinc-400/20 dark:hover:bg-white/20  ',
+  ]
+}
+
+const buttonItems = [
+  {
+    title: 'Single Choice',
+    description: 'One correct answer',
+    icon: scIcon,
+    type: questionTypes.SINGLE_CHOICE,
+  },
+  {
+    title: 'Multiple Choice',
+    description: 'Multiple correct answers',
+    icon: mcIcon,
+    type: questionTypes.MULTIPLE_CHOICE,
+  },
+  {
+    title: 'Judgement',
+    description: 'One answer, either true or false',
+    icon: tfIcon,
+    type: questionTypes.JUDGEMENT,
+  },
+]
+
+function QuestionTypeOption({ title, description, icon, onClick, ...props }) {
+  return (
+    <div>
+      <button type="button" onClick={onClick} className={clsx(styles.questionType, styles.default)} {...props}>
+        <div className="relative w-14 h-14 md:w-20 md:h-20 overflow-hidden bg-white/10 rounded-full">
+          <img src={icon} alt={title} className="absolute inset-0 object-cover w-full h-full" />
+        </div>
+        <div className="flex flex-col items-start justify-center flex-1">
+          <div className="w-full text-left">
+            <div className="flex flex-row justify-start gap-2">
+              <Heading className="text-white">{title}</Heading>
+            </div>
+            <Text className="text-base font-normal text-white/70 leading-none md:leading-normal">{description}</Text>
+          </div>
+        </div>
+      </button>
+    </div>
+  )
+}
+
+export default function NewQuestionModal({ isOpen, setIsOpen, createQuestion }) {
+  const handleClick = (questionType) => {
+    createQuestion(questionType)
+      .then(() => {
+        console.log('Question created');
+      })
+      .catch((error) => {
+        console.error('Error creating question:', error);
+      })
+      .finally(() => {
+        setIsOpen(false);
+      });
+  }
+
+  return (
+    <Dialog open={isOpen} onClose={setIsOpen}>
+      <DialogTitle>Add Question</DialogTitle>
+      <DialogDescription>
+        Select the type of question you want to add.
+      </DialogDescription>
+      <DialogBody>
+        <div className="flex flex-col gap-4">
+          {buttonItems.map((item) => (
+            <div key={item.title} className="flex flex-col">
+              <QuestionTypeOption
+                title={item.title}
+                description={item.description}
+                icon={item.icon}
+                onClick={() => handleClick(item.type)}
+              />
+            </div>
+          ))}
+        </div>
+      </DialogBody>
+      <DialogActions>
+        <Button plain onClick={() => setIsOpen(false)}>
+          Cancel
+        </Button>
+      </DialogActions>
+    </Dialog>
+  );
+}
--- frontend/src/game/index.jsx
+++ frontend/src/game/index.jsx
@@ -1,3 +1,4 @@
 export {default as EditGameForm} from './EditGameForm'
 export {default as EditQuestionForm} from './EditQuestionForm'
 export {default as EmptyState} from './EmptyState'
+export {default as NewQuestionModal} from './NewQuestionModal'
