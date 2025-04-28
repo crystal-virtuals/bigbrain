d6437b034d650892bac90cff828e2f9c7f3d5ca1
Crystal Huang <z5215571@ad.unsw.edu.au>
2025-04-29 04:20:45 +1000

    Edit BigBrain Game component

--- frontend/src/game/EditQuestionForm.jsx
+++ frontend/src/game/EditQuestionForm.jsx
@@ -1,59 +1,56 @@
 import { Button } from '@components/button';
 import { Card } from '@components/card';
-import { ConfirmModal, AlertModal} from '@components/modal';
-import { XCircleIcon } from '@heroicons/react/20/solid';
+import { AlertModal, ConfirmModal } from '@components/modal';
+import { InputQuestionAnswers, InputQuestionName, SelectDuration, SelectQuestionType, SelectPoints } from '@components/questions';
 import { PencilIcon, TrashIcon } from '@heroicons/react/24/solid';
-import { mapToQuestion, convertToQuestion, isEmptyQuestion, validateQuestion } from '@utils/game';
+import { convertToQuestion, isEmptyQuestion, mapToQuestion, validateQuestion } from '@utils/game';
+import clsx from 'clsx';
 import { useState } from 'react';
-import { InputQuestionAnswers, InputQuestionName, SelectQuestionType, SelectDuration } from '../question';
-import { pluralSuffix } from '@utils/helpers';
+import { ThumbnailInput } from '@components/form';
+import { useNavigate } from 'react-router-dom';
+import { Skeleton } from '@components/loading';
 
-export function FormAlert( { errors } ) {
+function QuestionThumbnail({ question, setQuestion }) {
+  const styles = [
+    'flex flex-col lg:justify-start gap-4 p-4 sm:rounded-xl',
+    'bg-zinc-100 dark:bg-zinc-800',
+    'shadow-xs ring-1 ring-zinc-900/5',
+  ];
+
+  const setThumbnail = (dataUrl) => {
+    setQuestion((prev) => ({ ...prev, thumbnail: dataUrl }));
+  }
+
   return (
-    <div className="rounded-md bg-red-50 p-4 col-span-full border-2 border-red-100">
-      <div className="flex">
-        <div className="shrink-0">
-          <XCircleIcon aria-hidden="true" className="size-5 text-red-400" />
-        </div>
-        <div className="ml-3">
-          <h3 className="text-sm font-medium text-red-800">There {errors.size === 1 ? 'is' : 'were'} {errors.size} error{pluralSuffix(errors.size)} with your submission</h3>
-          <div className="mt-2 text-sm text-red-700">
-            <ul role="list" className="list-disc space-y-1 pl-5">
-              {Array.from(errors).map(([key, value]) => (
-                <li key={key}>{value}</li>
-              ))}
-            </ul>
-          </div>
-        </div>
-      </div>
+    <div className={clsx(styles)}>
+      <ThumbnailInput
+        name="thumbnail"
+        value={question.thumbnail}
+        onChange={(dataUrl) => setThumbnail(dataUrl)}
+      />
     </div>
-  )
+  );
 }
 
-function EditQuestionForm({ question, deleteQuestion, onSubmit }) {
+function EditQuestionForm({ question, deleteQuestion, setErrors, onSubmit, isReadOnly = false }) {
   const [prevFormData, setPrevFormData] = useState(convertToQuestion(question));
   const [formData, setFormData] = useState(convertToQuestion(question));
-  const [errors, setErrors] = useState(new Map());
-  const [readOnly, setReadOnly] = useState(false);
+  const [readOnly, setReadOnly] = useState(isReadOnly);
   const [isConfirmOpen, setIsConfirmOpen] = useState(false);
   const [isDeleteOpen, setIsDeleteOpen] = useState(false);
   const [isSubmitting, setIsSubmitting] = useState(false);
-  const [showErrors, setShowErrors] = useState(false);
+  const navigate = useNavigate();
 
   const isEqual = (obj1, obj2) => {
     return JSON.stringify(obj1) === JSON.stringify(obj2);
   };
 
-  const validate = (question) => {
-    if (errors.size > 0) {
-      setShowErrors(true);
-      return false;
-    }
-    const validationErrors = validateQuestion(question);
+  const validate = () => {
+    const validationErrors = validateQuestion(formData);
+    console.log('Validation errors:', validationErrors);
     setErrors(validationErrors);
-    setShowErrors(true);
     return validationErrors.size === 0;
-  }
+  };
 
   const handleDelete = (e) => {
     e.preventDefault();
@@ -63,18 +60,12 @@ function EditQuestionForm({ question, deleteQuestion, onSubmit }) {
   const handleEdit = (e) => {
     e.preventDefault();
     setReadOnly(false);
-  }
+  };
 
   const handleCancel = (e) => {
     e.preventDefault();
-
     const isDirty = !isEqual(formData, prevFormData);
-    const isEmpty = isEmptyQuestion(prevFormData);
-
-    // If the question is empty, this is equivalent to deleting it
-    if (isEmpty) {
-      setIsDeleteOpen(true);
-    } else if (isDirty) {
+    if (isDirty) {
       setIsConfirmOpen(true);
     } else {
       discardChanges();
@@ -83,101 +74,132 @@ function EditQuestionForm({ question, deleteQuestion, onSubmit }) {
 
   const discardChanges = () => {
     setFormData(prevFormData);
-    setReadOnly(true);
     setIsConfirmOpen(false);
+    navigate(-1);
   };
 
   const deleteThisQuestion = () => {
-    deleteQuestion(question.id).finally(() => setIsDeleteOpen(false));
-  }
+    deleteQuestion(question.id)
+      .finally(() => {
+        setIsDeleteOpen(false)
+        // navigate back
+        navigate(-1);
+      });
+  };
 
   const handleSubmit = (e) => {
     e.preventDefault();
 
     // Validate form data
-    if (!validate(formData)) return;
+    if (!validate()) return;
 
     setIsSubmitting(true);
     const question = mapToQuestion(formData);
+
     onSubmit(question)
       .then(() => {
         console.log('Question updated successfully:', question);
         setPrevFormData(question);
         setFormData(question);
+        setErrors(new Map());
+        // navigate back
+        navigate(-1);
+
       })
       .catch((error) => {
         console.error('Error updating question:', error);
       })
       .finally(() => {
         setIsSubmitting(false);
-        setReadOnly(true);
       });
+  };
+
+  if (!question || !formData) {
+    return (
+      <div className="col-span-2 max-w-2xl">
+        <Skeleton className="h-96" />
+      </div>
+    );
   }
 
   return (
-    <Card className="mb-6">
-      <form onSubmit={handleSubmit}>
-        <div className="px-4 py-6 sm:p-8">
-          <div className="grid max-w-2xl grid-cols-1 gap-x-6 gap-y-8 sm:grid-cols-6">
-            {/* Form errors */}
-            {showErrors && errors.size > 0 && <FormAlert errors={errors} />}
+    <>
+      {/* Question Thumbnail */}
+      <QuestionThumbnail question={formData} setQuestion={setFormData}/>
 
-            {/* Question name */}
-            <InputQuestionName
-              question={formData}
-              setQuestion={setFormData}
-              readOnly={readOnly}
-              errors={errors}
-              setErrors={setErrors}
-            />
+      {/* Question Form */}
+      <Card>
+        <form onSubmit={handleSubmit}>
+          <div className="px-4 py-6 sm:p-8">
+            <div className="grid max-w-2xl grid-cols-1 gap-x-6 gap-y-8 sm:grid-cols-6">
+              {/* Question name */}
+              <InputQuestionName
+                question={formData}
+                setQuestion={setFormData}
+              />
 
-            {/* Question answers */}
-            <InputQuestionAnswers
-              question={formData}
-              setQuestion={setFormData}
-              answers={formData.answers}
-              setAnswers={(answers) => setFormData(prev => ({ ...prev, answers }))}
-              readOnly={readOnly}
-              errors={errors}
-              setErrors={setErrors}
-              invalid={errors.has('answers') && !readOnly}
-            />
+              {/* Question answers */}
+              <InputQuestionAnswers
+                type={formData.type}
+                answers={formData.answers}
+                setAnswers={(answers) =>
+                  setFormData((prev) => ({ ...prev, answers }))
+                }
+              />
 
-            {/* Question type */}
-            <SelectQuestionType question={formData} setQuestion={setFormData} readOnly={readOnly} />
+              {/* Question type */}
+              <SelectQuestionType
+                question={formData}
+                setQuestion={setFormData}
+                resetForm={() => setErrors(new Map())}
+              />
 
-            {/* Time limit */}
-            <SelectDuration question={formData} setQuestion={setFormData} readOnly={readOnly} />
+              {/* Time limit */}
+              <SelectDuration question={formData} setQuestion={setFormData} />
 
+              {/* Points */}
+              <SelectPoints question={formData} setQuestion={setFormData} />
+            </div>
           </div>
-        </div>
 
-        <div className="flex items-center justify-end gap-x-6 border-t border-zinc-900/10 dark:border-white/10 px-4 py-4 sm:px-8">
-          {readOnly ? (
-            <>
-              <Button type="button" color='red' onClick={handleDelete}>
-                <TrashIcon aria-hidden="true" />
-                Delete
-              </Button>
+          <div className="flex items-center justify-end gap-x-6 border-t border-zinc-900/10 dark:border-white/10 px-4 py-4 sm:px-8">
+            {readOnly ? (
+              <>
+                <Button type="button" color="red" onClick={handleDelete}>
+                  <TrashIcon aria-hidden="true" />
+                  Delete
+                </Button>
 
-              <Button type="button" color='white' onClick={handleEdit}>
-                <PencilIcon aria-hidden="true" />
-                Edit
-              </Button>
-            </>
-          ) : (
-            <>
-              <Button type="button" onClick={handleCancel} disabled={isSubmitting} outline>
-                Cancel
-              </Button>
+                <Button type="button" color="white" onClick={handleEdit}>
+                  <PencilIcon aria-hidden="true" />
+                  Edit
+                </Button>
+              </>
+            ) : (
+              <>
+                <Button
+                  type="button"
+                  onClick={handleCancel}
+                  disabled={isSubmitting}
+                  outline
+                >
+                  Cancel
+                </Button>
 
-              <Button type="submit" onClick={handleSubmit} loading={isSubmitting} disabled={isSubmitting} color='teal'>
-                Save
-              </Button>
-            </>
-          )}
-        </div>
-      </form>
+                <Button
+                  type="submit"
+                  onClick={handleSubmit}
+                  loading={isSubmitting}
+                  disabled={isSubmitting}
+                  color="teal"
+                >
+                  Save
+                </Button>
+              </>
+            )}
+          </div>
+        </form>
+      </Card>
 
       {/* Confirm Dialog */}
       <ConfirmModal
@@ -199,8 +221,7 @@ function EditQuestionForm({ question, deleteQuestion, onSubmit }) {
         setIsOpen={setIsDeleteOpen}
         onConfirm={deleteThisQuestion}
       />
-    </Card>
-
+    </>
   );
 }
 
