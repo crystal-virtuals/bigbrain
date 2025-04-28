46b460b63ad4754b0eb31bd8c70ddf0eb938a415
Crystal Huang <z5215571@ad.unsw.edu.au>
2025-04-29 04:20:39 +1000

    Edit BigBrain Game Question component

--- frontend/src/admin/index.jsx
+++ frontend/src/admin/index.jsx
@@ -2,3 +2,4 @@ export { default as AdminLayout } from './AdminLayout';
 export { default as Dashboard } from './Dashboard';
 export { default as EditGame } from './EditGame';
 export { default as EditQuestion } from './EditQuestion';
+export { default as GameLayout } from './GameLayout';
--- frontend/src/game/EditGameForm.jsx
+++ frontend/src/game/EditGameForm.jsx
@@ -1,4 +1,3 @@
-import { Card } from '@components/card';
 import { Field, Label } from '@components/fieldset';
 import { EditForm, useEditForm } from '@hooks/form';
 import { FileInput, TextInput } from '@components/form';
@@ -13,9 +12,8 @@ const validate = (game) => {
   return null;
 }
 
-
-function EditGameForm({ game, onSubmit }) {
-  const props = useEditForm(mapToGame(game), validate, onSubmit);
+function EditGameForm({ game, onSubmit, isReadOnly = false }) {
+  const props = useEditForm(mapToGame(game), onSubmit, validate, isReadOnly);
   const { formData, setFormData, ...rest } = props;
 
   const handleChange = (field, value) => {
@@ -24,43 +22,37 @@ function EditGameForm({ game, onSubmit }) {
 
   // Make sure game and formData has been set
   if (isNullOrUndefined(formData)) {
-    return (
-      <Card>
-        <Skeleton className="col-span-full max-w-2xl" />
-      </Card>
-    );
+    return <Skeleton className="col-span-full max-w-2xl" />;
   }
 
   return (
     <>
-      <Card>
-        <EditForm {...rest}>
-          {/* Game name */}
-          <Field className="col-span-full">
-            <Label htmlFor="gameName">Game name</Label>
-            <TextInput
-              id="gameName"
-              name="name"
-              value={formData.name}
-              onChange={(value) => handleChange('name', value)}
-              readOnly={props.readOnly}
-              disabled={props.disabled}
-            />
-          </Field>
+      <EditForm {...rest}>
+        {/* Game name */}
+        <Field className="col-span-full">
+          <Label htmlFor="gameName">Game name</Label>
+          <TextInput
+            id="gameName"
+            name="name"
+            value={formData.name}
+            onChange={(value) => handleChange('name', value)}
+            readOnly={props.readOnly}
+            disabled={props.disabled}
+          />
+        </Field>
 
-          {/* Game thumbnail */}
-          <Field className="col-span-full">
-            <Label htmlFor="thumbnail">Photo</Label>
-            <FileInput
-              name="thumbnail"
-              value={formData.thumbnail}
-              onChange={(dataUrl) => handleChange('thumbnail', dataUrl)}
-              readOnly={props.readOnly}
-              disabled={props.disabled}
-            />
-          </Field>
-        </EditForm>
-      </Card>
+        {/* Game thumbnail */}
+        <Field className="col-span-full">
+          <Label htmlFor="thumbnail">Photo</Label>
+          <FileInput
+            name="thumbnail"
+            value={formData.thumbnail}
+            onChange={(dataUrl) => handleChange('thumbnail', dataUrl)}
+            readOnly={props.readOnly}
+            disabled={props.disabled}
+          />
+        </Field>
+      </EditForm>
     </>
   );
 }
