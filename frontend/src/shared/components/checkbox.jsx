0d04c75f1e6cd7035a83aed7ebfd73b831a4edb5
Crystal Huang <z5215571@ad.unsw.edu.au>
2025-05-05 21:09:53 +1000

    Added multiple choice checkbox button

--- frontend/src/shared/components/checkbox.jsx
+++ frontend/src/shared/components/checkbox.jsx
@@ -1,5 +1,6 @@
-import * as Headless from '@headlessui/react'
-import clsx from 'clsx'
+import * as Headless from '@headlessui/react';
+import clsx from 'clsx';
+import { CheckCircleIcon, XCircleIcon } from '@heroicons/react/16/solid';
 
 export function CheckboxGroup({ className, ...props }) {
   return (
@@ -14,7 +15,7 @@ export function CheckboxGroup({ className, ...props }) {
         'has-data-[slot=description]:space-y-6 has-data-[slot=description]:**:data-[slot=label]:font-medium'
       )}
     />
-  )
+  );
 }
 
 export function CheckboxField({ className, ...props }) {
@@ -36,7 +37,7 @@ export function CheckboxField({ className, ...props }) {
         'has-data-[slot=description]:**:data-[slot=label]:font-medium'
       )}
     />
-  )
+  );
 }
 
 const base = [
@@ -65,7 +66,7 @@ const base = [
   // Forced colors mode
   'forced-colors:[--checkbox-check:HighlightText] forced-colors:[--checkbox-checked-bg:Highlight] forced-colors:group-data-disabled:[--checkbox-check:Highlight]',
   'dark:forced-colors:[--checkbox-check:HighlightText] dark:forced-colors:[--checkbox-checked-bg:Highlight] dark:forced-colors:group-data-disabled:[--checkbox-check:Highlight]',
-]
+];
 
 const colors = {
   'dark/zinc': [
@@ -106,7 +107,7 @@ const colors = {
     '[--checkbox-check:var(--color-white)] [--checkbox-checked-bg:var(--color-fuchsia-500)] [--checkbox-checked-border:var(--color-fuchsia-600)]/90',
   pink: '[--checkbox-check:var(--color-white)] [--checkbox-checked-bg:var(--color-pink-500)] [--checkbox-checked-border:var(--color-pink-600)]/90',
   rose: '[--checkbox-check:var(--color-white)] [--checkbox-checked-bg:var(--color-rose-500)] [--checkbox-checked-border:var(--color-rose-600)]/90',
-}
+};
 
 export function Checkbox({ color = 'dark/zinc', className, ...props }) {
   return (
@@ -140,5 +141,140 @@ export function Checkbox({ color = 'dark/zinc', className, ...props }) {
         </svg>
       </span>
     </Headless.Checkbox>
-  )
+  );
+}
+
+/***************************************************************
+                      Checkbox Button
+***************************************************************/
+export function CheckboxButtonGroup({ className, ...props }) {
+  return (
+    <div
+      data-slot="control"
+      {...props}
+      className={clsx(
+        className,
+        // Basic groups
+        'flex flex-col items-center w-full space-y-2 mt-4 max-w-lg px-4',
+      )}
+    />
+  );
+}
+
+export function CheckboxButtonField({ className, children, correct, ...props }) {
+  return (
+    <Headless.Field
+      data-slot="field"
+      {...props}
+      className={clsx(
+        className,
+        // Base layout
+        'flex flex-row justify-center w-full',
+      )}
+    >
+      <div className="relative w-full">
+        {children}
+        {correct === true && (
+          <div className="top-1/2 translate-x-1/4 absolute right-0 z-10 overflow-hidden transform -translate-y-1/2 size-6 rounded-full bg-white">
+            <CheckCircleIcon className="fill-green-600 size-6" />
+          </div>
+        )}
+        {correct === false && (
+          <div className="top-1/2 translate-x-1/4 absolute right-0 z-10 overflow-hidden transform -translate-y-1/2 size-6 rounded-full bg-white">
+            <XCircleIcon className="fill-error size-6" />
+          </div>
+        )}
+      </div>
+    </Headless.Field>
+  );
+}
+
+export function CheckboxButton({ className, children, ...props }) {
+  const main = [
+    // Base
+    'group relative flex w-full touch-manipulation cursor-pointer',
+    // Disabled
+    'data-disabled:pointer-events-none',
+    // Cursor
+    'cursor-pointer touch-manipulation pointer-events-auto',
+  ];
+
+  const base = [
+    'size-8 overflow-hidden',
+    'relative isolate flex items-center justify-center w-full h-full',
+    'bg-gray-content/90',
+    'border-4 border-solid rounded-md border-gray',
+    // Disabled state
+    'group-data-disabled:border-zinc-950/25 group-data-disabled:bg-zinc-950/5',
+  ];
+
+  const content = (
+    <>
+      <div className="-inset-1 absolute z-0 rounded-md" />
+      <div className="absolute inset-x-0 top-0 bottom-0 transform group-active:translate-y-0.5 group-active:bottom-0.5 z-1 bg-black rounded-[0.65rem] p-[0.1875rem]">
+        <div className="relative w-full h-full">
+          <div className="top-1 absolute inset-x-0 bottom-0 overflow-hidden rounded-[0.4375rem] bg-beige">
+            <div className="opacity-30 absolute inset-0 bg-black" />
+          </div>
+          <div className="bottom-1 absolute inset-x-0 top-0 overflow-hidden group-active:bottom-0.5 rounded-[0.4375rem] bg-beige">
+            <div className="group-hover:opacity-20 absolute inset-0 opacity-0" />
+          </div>
+        </div>
+      </div>
+      <div className="z-1 absolute inset-0 overflow-hidden hidden rounded-[0.4375rem] ">
+        <div
+          className="opacity-20 absolute top-0 left-0 w-full h-full bg-black"
+          style={{ left: '-100%' }}
+        />
+      </div>
+    </>
+  );
+
+  return (
+    <Headless.Checkbox
+      data-slot="control"
+      {...props}
+      className={clsx(className, main)}
+    >
+      <button
+        className="relative flex group touch-manipulation cursor-pointer px-5 w-full pointer-events-auto leading-none group-data-disabled:pointer-events-none"
+        type="button"
+      >
+        {/* Background layers */}
+        {content}
+        {/* Checkbox indicator */}
+        <div className="relative flex flex-row gap-x-4 items-center w-full min-h-full pointer-events-none z-2 transform -translate-y-0.5 group-active:translate-y-0 p-[0.25rem]">
+          <div className="relative flex-grow-0">
+            <div className={clsx(base)}>
+              <div className="relative flex items-center justify-center w-full h-full">
+                {/* Checkmark icon */}
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  viewBox="0 0 40 40"
+                  className="size-4 stroke-black opacity-0 group-data-checked:opacity-100"
+                >
+                  <path
+                    fill="black"
+                    className="opacity-100 group-data-indeterminate:opacity-0"
+                    d="m14.6 33.11-8.31-10a2.88 2.88 0 0 1-.82-2 2.68 2.68 0 0 1 2.8-2.74 2.66 2.66 0 0 1 2.22 1.07l6.61 8.1L30.14 7a2.63 2.63 0 0 1 2.46-1.48 2.7 2.7 0 0 1 2.81 2.73 3.32 3.32 0 0 1-.62 1.86L19.79 33a3 3 0 0 1-2.63 1.34 3.15 3.15 0 0 1-2.56-1.23z"
+                  />
+                </svg>
+              </div>
+            </div>
+          </div>
+
+          {/* Content */}
+          <div className="flex flex-col flex-1 items-start">
+            <div className="relative">
+              <div className="relative text-left">
+                <div className="md:py-4 text-base text-black font-bold">
+                  {children}
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </button>
+    </Headless.Checkbox>
+  );
 }
