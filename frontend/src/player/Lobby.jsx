7854e02873d4e5bfe4f37b2249e1aa85f1f0a09a
Crystal Huang <z5215571@ad.unsw.edu.au>
2025-05-11 21:09:56 +1000

    Continued work on Player Session

---
+++ frontend/src/player/Lobby.jsx
@@ -0,0 +1,35 @@
+import { LobbyLayout, LobbyMainSection } from '@components/session/lobby';
+import { Heading } from '@components/heading';
+import { ButtonPrimary as Button } from '@components/button';
+import { useSession } from '@hooks/session';
+
+function Lobby({ sessionId, status }) {
+  const { player } = useSession();
+  return (
+    <LobbyLayout>
+      <LobbyMainSection
+        title="You have joined with Session ID:"
+        sessionId={sessionId}
+      >
+        <div className="flex items-center justify-center flex-1">
+          <Heading className="flex items-end gap-2">
+            Joining as {player.name}
+          </Heading>
+        </div>
+        <Button disabled={!status} color="light">
+          <div className="flex items-end gap-2">
+            <span className="text-base text-neutral hidden md:block">
+              Waiting for host to start
+            </span>
+            <span className="text-base text-neutral block md:hidden">
+              Waiting
+            </span>
+            <span className="loading loading-dots loading-sm" />
+          </div>
+        </Button>
+      </LobbyMainSection>
+    </LobbyLayout>
+  );
+}
+
+export default Lobby;
